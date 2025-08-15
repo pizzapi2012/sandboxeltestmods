5981b3e8881e4d49ceb2ad6b7d0f7321c9048703
Joseph Knutson <acrazypencil123@gmail.com>
2025-08-15 08:38:50 -0500

    commit

---
+++ test_mod.js
@@ -0,0 +1,14 @@
+elements.testing_element = {
+    color: "#c07a1eff",
+    behavior: [
+        "XX|XX|XX",
+        "XX|XX|M1",
+        "XX|XX|XX"
+    ],
+    category: "testing",
+    state: "solid",
+    stateHigh: "carbon_dioxide",
+    tempHigh: 100,
+    tempLow: 0,
+    stateLow: "ice"
+}
